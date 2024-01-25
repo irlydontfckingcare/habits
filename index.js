7cb822fb450c25c9ab8136b780997add7906acd3
Ryan Reynolds <lloydkliebe77@gmail.com>
2024-01-25 04:57:12 +0100

    design

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function alerta() {
     let inputTxt = document.getElementById('habit-input').value
     console.log("a: " + inputTxt)
-    if (inputTxt.length == 0) 
+    if (inputTxt.length == 0)
         alert("Kannste ned schreiben?!")
-     else 
+    else
         alert("du magst " + inputTxt.toUpperCase());
 }
