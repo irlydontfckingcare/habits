dd66009b71df1b5952c4b96a9c1db27fd8c93e9f
Ryan Reynolds <lloydkliebe77@gmail.com>
2024-01-26 21:05:44 +0100

    Button Logik

--- index.js
+++ index.js
@@ -1,8 +1,82 @@
+let dayWasSelected = false;
+let nightWasSelected = false;
+let daytimeButtonElement;
+let nighttimeButtonElement;
+
+function init() {
+
+    initNightTimeButton();
+    initDayTimeButton();
+
+}
+
+function initDayTimeButton() {
+
+    nighttimeButtonElement = document.getElementById('daytime-button-night');
+
+    nighttimeButtonElement.addEventListener('click', function () {
+
+        if (dayWasSelected) deactivateDay();
+
+        if (!nightWasSelected) {
+            activateNight();
+            return;
+        }
+
+        deactivateNight();
+
+    });
+}
+
+function initNightTimeButton() {
+
+    daytimeButtonElement = document.getElementById('daytime-button-day');
+
+    daytimeButtonElement.addEventListener('click', function () {
+
+        if (nightWasSelected) deactivateNight();
+
+        if (!dayWasSelected) {
+            activateDay();
+            return;
+        }
+
+        deactivateDay();
+
+    });
+}
+
+function activateNight() {
+    nightWasSelected = true;
+    setBackgroundColorOfElement(nighttimeButtonElement, 'red');
+}
+
+function activateDay() {
+    dayWasSelected = true;
+    setBackgroundColorOfElement(daytimeButtonElement, 'red');
+}
+
+function deactivateDay() {
+    dayWasSelected = false;
+    setBackgroundColorOfElement(daytimeButtonElement, 'lightblue');
+}
+
+function deactivateNight() {
+    nightWasSelected = false;
+    setBackgroundColorOfElement(nighttimeButtonElement, 'lightblue');
+}
+
+function setBackgroundColorOfElement(element, color) {
+    element.setAttribute('style', 'background-color:' + color);
+}
+
+
 function alerta() {
-    let inputTxt = document.getElementById('habit-input').value
-    console.log("a: " + inputTxt)
-    if (inputTxt.length == 0)
-        alert("Kannste ned schreiben?!")
-    else
-        alert("du magst " + inputTxt.toUpperCase());
+    alert("moin")
+    let habit = {
+        "key": 1,
+        "activity": "Laufen",
+        "day": true,
+    }
+    localStorage.setItem('01', JSON.stringify(habit));
 }
